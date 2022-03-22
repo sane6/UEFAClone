426b6471692772f47e901296eb6b43fc1b6eacb4
srki.sanadrovic@gmail.com <srki.sanadrovic@gmail.com>
2022-03-22 12:10:25 +0100

    field potential bugs

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,14 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import field from '../assets/images/field.jpg';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground source={field} resizeMode='contain' style={{width: '100%', aspectRatio: 2 / 3}}></ImageBackground>
+    </SafeAreaView>
   );
 }
 
@@ -18,15 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
