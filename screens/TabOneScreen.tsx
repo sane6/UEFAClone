fbec26df4d6c60798225a35b53975d5277052a77
Srđan Sanadrović <srki.sanadrovic@gmail.com>
2022-03-23 11:22:02 +0100

    view players button

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, SafeAreaView } from 'react-native'
+import { StyleSheet, SafeAreaView, Pressable, Text } from 'react-native'
 import Field from '../components/Field'
 
 const players: { [key: string]: null[] } = {
@@ -9,9 +9,16 @@ const players: { [key: string]: null[] } = {
 }
 
 export default function TabOneScreen() {
+	const viewPlayers = () => {
+		console.warn('View Players')
+	}
+
 	return (
 		<SafeAreaView style={styles.container}>
 			<Field />
+			<Pressable onPress={viewPlayers} style={styles.buttonContainer}>
+				<Text>View Players</Text>
+			</Pressable>
 		</SafeAreaView>
 	)
 }
@@ -21,5 +28,14 @@ const styles = StyleSheet.create({
 		// paddingTop: Platform.OS === 'android' ? 25 : 0,
 		flex: 1,
 		alignItems: 'center',
+	},
+	buttonContainer: {
+		backgroundColor: 'orange',
+		width: '90%',
+		margin: 20,
+		marginTop: 'auto',
+		padding: 10,
+		alignItems: 'center',
+		borderRadius: 50,
 	},
 })
